79e455d71ceda94be071dccc5ad37e0442074602
nickluong-dev <nickluong97@outlook.com>
2025-02-12 11:37:55 -0800

    enh: log build.version
    Co-authored-by: Norbert van Nobelen <norbertvannobelen@users.noreply.github.com>

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,8 +2,27 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router";
 
 import { Toaster } from "./components/Toaster";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetch("/build.version")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `Failed to load build.version: ${response.statusText}`
+          );
+        }
+        return response.text();
+      })
+      .then((versionText) => {
+        console.log("Build version:", versionText);
+      })
+      .catch((error) => {
+        console.error("Error fetching build version:", error);
+      });
+  }, []);
+
   return (
     <>
       <Toaster />
