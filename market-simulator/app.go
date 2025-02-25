03c3af4ce31d07091fe47864251f271bafe21e8a
Norbert van Nobelen <norbert.van.nobelen2@gmail.com>
2025-02-25 13:04:02 -0800

    sine curve simulation for demo data

--- market-simulator/app.go
+++ market-simulator/app.go
@@ -53,6 +53,19 @@ const (
 	maxSDKIntWordLen = math.MaxBitLen / bits.UintSize
 )
 
+type currency struct {
+	name     string
+	currency string
+}
+
+/*
+array with 2 elements of currency-issuer
+*/
+var (
+	currencyArray = []currency{
+		{"NOR", "nor"}, {"ALB", "alb"}}
+)
+
 type App struct {
 	cfg AppConfig
 
@@ -62,6 +75,7 @@ type App struct {
 	sides     []dextypes.Side
 	clientCtx client.Context
 	txFactory tx.Factory
+	iteration int // Track iteration count
 }
 
 func NewApp(
@@ -115,7 +129,7 @@ func NewApp(
 
 	denoms := make([]string, 0)
 	denoms = append(denoms, lo.RepeatBy(cfg.AssetFTDefaultDenomsCount, func(i int) string {
-		denom := fmt.Sprintf("dextestdenom%d-%s", i, issuer.String())
+		denom := denom(i)
 		supply, err := bankClient.SupplyOf(ctx, &banktypes.QuerySupplyOfRequest{Denom: denom})
 		if err != nil {
 			panic(err)
@@ -123,10 +137,10 @@ func NewApp(
 		if supply.Amount.IsZero() {
 			issueMsg := &assetfttypes.MsgIssue{
 				Issuer:        issuer.String(),
-				Symbol:        fmt.Sprintf("DexTestDenom0%d", i),
-				Subunit:       fmt.Sprintf("dextestdenom0%d", i),
+				Symbol:        currencyArray[i].name,
+				Subunit:       currencyArray[i].currency,
 				Precision:     6,
-				Description:   "Dex Test Denom",
+				Description:   currencyArray[i].name,
 				InitialAmount: math.NewInt(1000000000000),
 				Features: []assetfttypes.Feature{
 					assetfttypes.Feature_minting,
@@ -140,7 +154,7 @@ func NewApp(
 				issueMsg,
 			)
 			if err != nil {
-				panic(err)
+				slog.Info("Error issuing %s: %s", denom, err)
 			}
 		}
 
@@ -194,13 +208,14 @@ func addToKeyring(clientCtx client.Context, item AccountWallet) types.AccAddress
 func (fa *App) CreateOrder(
 	ctx context.Context,
 	rootRnd *rand.Rand,
-	sender types.AccAddress,
+	accounts []types.AccAddress,
 ) error {
 	startTime := time.Now()
 	orderSeed := rootRnd.Int63()
 	orderRnd := rand.New(rand.NewSource(orderSeed))
 
-	msgIssue, msgPlaceOrder, err := fa.GenOrder(orderRnd, sender)
+	// One side always sells, the other side always buys
+	msgIssueSell, msgIssueBuy, msgPlaceSellOrder, msgPlaceBuyOrder, err := fa.GenOrder(orderRnd, accounts)
 	if err != nil {
 		return err
 	}
@@ -209,28 +224,65 @@ func (fa *App) CreateOrder(
 		ctx,
 		fa.clientCtx.WithFromAddress(fa.issuer),
 		fa.txFactory,
-		msgIssue,
+		msgIssueSell,
 	)
 	if err != nil {
-		slog.Error("broadcastTX", slog.String("account", msgIssue.Sender), slog.String("denom", msgIssue.Coin.Denom), slog.String("Error:", err.Error()))
+		slog.Error("broadcastTX",
+			slog.String("account", msgIssueSell.Sender),
+			slog.String("denom", msgIssueSell.Coin.Denom),
+			slog.String("Error:", err.Error()))
 		return err
 	}
 
+	_, err = client.BroadcastTx(
+		ctx,
+		fa.clientCtx.WithFromAddress(fa.issuer),
+		fa.txFactory,
+		msgIssueBuy,
+	)
+	if err != nil {
+		slog.Error("broadcastTX",
+			slog.String("account", msgIssueBuy.Sender),
+			slog.String("denom", msgIssueBuy.Coin.Denom),
+			slog.String("Error:", err.Error()))
+		return err
+	}
 	res, err := client.BroadcastTx(
 		ctx,
-		fa.clientCtx.WithFromAddress(sender),
+		fa.clientCtx.WithFromAddress(accounts[0]),
 		fa.txFactory,
-		msgPlaceOrder,
+		msgPlaceSellOrder,
 	)
 	if err != nil {
 		if strings.Contains(err.Error(), "it's prohibited to save more than 100 orders per denom") {
-			slog.Error("it's prohibited to save more than 100 orders per denom", slog.String("account", msgPlaceOrder.Sender), slog.String("denom", msgPlaceOrder.BaseDenom))
+			slog.Error("it's prohibited to save more than 100 orders per denom",
+				slog.String("account", msgPlaceSellOrder.Sender),
+				slog.String("denom", msgPlaceSellOrder.BaseDenom))
 			return err
 		}
 		return err
 	}
 
-	slog.Info("new order", slog.Any("TX hash", res.TxHash), slog.Int64("Block Height", res.Height), slog.Int64("Gas Used", res.GasUsed), slog.Any("order", msgPlaceOrder))
+	slog.Info("new order", slog.Any("TX hash", res.TxHash),
+		slog.Int64("Block Height", res.Height), slog.Int64("Gas Used", res.GasUsed), slog.Any("order", msgPlaceSellOrder))
+
+	res, err = client.BroadcastTx(
+		ctx,
+		fa.clientCtx.WithFromAddress(accounts[1]),
+		fa.txFactory,
+		msgPlaceBuyOrder,
+	)
+	if err != nil {
+		if strings.Contains(err.Error(), "it's prohibited to save more than 100 orders per denom") {
+			slog.Error("it's prohibited to save more than 100 orders per denom",
+				slog.String("account", msgPlaceBuyOrder.Sender),
+				slog.String("denom", msgPlaceBuyOrder.BaseDenom))
+			return err
+		}
+		return err
+	}
+	slog.Info("new order", slog.Any("TX hash", res.TxHash),
+		slog.Int64("Block Height", res.Height), slog.Int64("Gas Used", res.GasUsed), slog.Any("order", msgPlaceBuyOrder))
 
 	took := time.Since(startTime)
 	slog.Info(fmt.Sprintf("broadcasting order took %s\n", took.String()))
@@ -241,13 +293,92 @@ func (fa *App) GetAccounts() []types.AccAddress {
 	return fa.accounts
 }
 
-func (fa *App) GenPair(rnd *rand.Rand) (string, string) {
-	// take two denoms for single market
-	return fa.denoms[0], fa.denoms[1]
+func (fa *App) GenOrder(rnd *rand.Rand, accounts []types.AccAddress) (*assetfttypes.MsgMint,
+	*assetfttypes.MsgMint, *dextypes.MsgPlaceOrder, *dextypes.MsgPlaceOrder, error) {
+	baseDenom, quoteDenom := fa.denoms[0], fa.denoms[1]
+
+	// Sine wave-based pricing
+	t := float64(fa.iteration)          // Use iteration as time step
+	priceValue := 90 + 10*gomath.Sin(t) // Base price 90, oscillating ±10
+	// Round priceValue to nearest integer
+	priceValue = gomath.Round(priceValue)
+	priceNum := uint64(priceValue)
+	var priceExp int8 = 0
+
+	price, ok := buildNumExpPrice(priceNum, priceExp)
+	if !ok {
+		return nil, nil, nil, nil, fmt.Errorf("could not parse %de%d as price", priceNum, priceExp)
+	}
+
+	quantity := 10 * int64(gomath.Pow(10, 6))
+	coinsToMint := types.NewCoin(baseDenom, math.NewInt(quantity))
+
+	sellOrder := &dextypes.MsgPlaceOrder{
+		Sender:     accounts[0].String(),
+		Type:       dextypes.ORDER_TYPE_LIMIT,
+		ID:         uuid.New().String(),
+		BaseDenom:  baseDenom,
+		QuoteDenom: quoteDenom,
+		Price:      &price,
+		Quantity:   math.NewInt(quantity),
+		Side:       dextypes.SIDE_SELL,
+		GoodTil: &dextypes.GoodTil{
+			GoodTilBlockHeight: 0,
+			GoodTilBlockTime:   lo.ToPtr(time.Now().Add(time.Hour)),
+		},
+		TimeInForce: dextypes.TIME_IN_FORCE_GTC,
+	}
+	mintSell := &assetfttypes.MsgMint{
+		Sender:    fa.issuer.String(),
+		Coin:      coinsToMint,
+		Recipient: accounts[0].String(),
+	}
+
+	priceValue = 90 + 10*gomath.Sin(float64(fa.iteration-1))
+	priceValue = gomath.Round(priceValue)
+	priceNum = uint64(priceValue)
+	price, _ = buildNumExpPrice(priceNum, priceExp)
+	amount, err := mulCeil(math.NewInt(quantity), price)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+	coinsToMint = types.NewCoin(quoteDenom, amount)
+
+	buyOrder := &dextypes.MsgPlaceOrder{
+		Sender:     accounts[0].String(),
+		Type:       dextypes.ORDER_TYPE_LIMIT,
+		ID:         uuid.New().String(),
+		BaseDenom:  baseDenom,
+		QuoteDenom: quoteDenom,
+		Price:      &price,
+		Quantity:   math.NewInt(quantity),
+		Side:       dextypes.SIDE_BUY,
+		GoodTil: &dextypes.GoodTil{
+			GoodTilBlockHeight: 0,
+			GoodTilBlockTime:   lo.ToPtr(time.Now().Add(time.Hour)),
+		},
+		TimeInForce: dextypes.TIME_IN_FORCE_GTC,
+	}
+	mintBuy := &assetfttypes.MsgMint{
+		Sender:    fa.issuer.String(),
+		Coin:      coinsToMint,
+		Recipient: accounts[1].String(),
+	}
+	// Increment iteration for next order
+	fa.iteration++
+	// Every 1000 iterations fund the accounts (way sufficient to keep running)
+	if fa.iteration%1000 == 0 {
+		addFund(accounts[0].String())
+		addFund(accounts[1].String())
+	}
+	return mintSell,
+		mintBuy,
+		sellOrder,
+		buyOrder, nil
 }
 
-func (fa *App) GenOrder(rnd *rand.Rand, sender types.AccAddress) (*assetfttypes.MsgMint, *dextypes.MsgPlaceOrder, error) {
-	baseDenom, quoteDenom := fa.GenPair(rnd)
+func (fa *App) GenOrder2(rnd *rand.Rand, sender types.AccAddress) (*assetfttypes.MsgMint, *dextypes.MsgPlaceOrder, error) {
+	baseDenom, quoteDenom := fa.denoms[0], fa.denoms[1]
 	side := getAnyItemByIndex(fa.sides, rnd.Intn(len(fa.sides)))
 
 	priceNum := randIntInRange(rnd, 80, 100)
@@ -343,4 +474,8 @@ func isBigIntOverflowsSDKInt(i *big.Int) bool {
 		return i.BitLen() > math.MaxBitLen
 	}
 	return false
+}
+
+func denom(denom int) string {
+	return fmt.Sprintf("%s-%s", currencyArray[denom].currency, "devcore19p7572k4pj00szx36ehpnhs8z2gqls8ky3ne43")
 }
