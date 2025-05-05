17a137ffd754db4d778e7147640f70ffd80be5cb
nickluong-dev <nickluong97@outlook.com>
2025-05-05 12:42:58 -0700

    fix: refresh app on timeout to prevent stale websocket

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import { Toaster } from "./components/Toaster";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { wsManager } from "./services/websocket";
 import { WS_URL } from "./config/envs";
 import { useStore } from "./state/store";
@@ -11,6 +11,37 @@ import { ICoreumWallet } from "./types/market";
 function App() {
   const { wallet, network, setWallet, setCoreum, pushNotification } =
     useStore();
+
+  const lastActiveTime = useRef(Date.now());
+  const INACTIVE_THRESHOLD = 10 * 60 * 1000; // 10mins
+
+  useEffect(() => {
+    const handleVisibilityChange = () => {
+      if (document.visibilityState === "visible") {
+        const now = Date.now();
+        const inactiveTime = now - lastActiveTime.current;
+        console.log(
+          `Tab was inactive for ${inactiveTime / 1000} seconds (threshold: ${
+            INACTIVE_THRESHOLD / 1000
+          } seconds)`
+        );
+
+        if (inactiveTime > INACTIVE_THRESHOLD) {
+          console.log("Refreshing page now");
+          window.location.reload();
+        }
+      } else {
+        lastActiveTime.current = Date.now();
+        console.log(`Set lastActiveTime to ${lastActiveTime.current}`);
+      }
+    };
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    };
+  }, []);
 
   useEffect(() => {
     fetch("/build.version")
