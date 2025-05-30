65149241fa8ba0cb310a1ed7bd7ae2b10d71f981
nickluong-dev <nickluong97@outlook.com>
2025-05-30 13:24:50 -0700

    fix: prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -5,6 +5,7 @@ set -xe
 for variable in \
     VITE_ENV_BASE_API \
     VITE_ENV_WS \
+    VITE_ENV_DEFAULT_MARKET_CONFIGS \
     VITE_ALLOWED_HOST ; do
   eval value="\$${variable}"
   echo ${value}
