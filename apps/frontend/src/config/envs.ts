0f71185878920372d86357f915ae8c034ee2e9a0
nickluong-dev <66899610+nickluong-dev@users.noreply.github.com>
2025-05-30 13:08:09 -0700

    fix: reset history state on market change to not show old market data… (#73)
    * fix: reset history state on market change to not show old market data, refetch on market change
    
    * fix: properly use default market config for staging and dev
    
    * fix: properly use default market config for staging and dev

--- apps/frontend/src/config/envs.ts
+++ apps/frontend/src/config/envs.ts
@@ -1,6 +1,7 @@
 interface CoreumEnv {
   VITE_ENV_BASE_API: string;
   VITE_ENV_WS: string;
+  VITE_ENV_DEFAULT_MARKET_CONFIGS: string;
 }
 
 const env: CoreumEnv =
@@ -8,8 +9,11 @@ const env: CoreumEnv =
     ? {
         VITE_ENV_BASE_API: import.meta.env.VITE_ENV_BASE_API,
         VITE_ENV_WS: import.meta.env.VITE_ENV_WS,
+        VITE_ENV_DEFAULT_MARKET_CONFIGS: import.meta.env
+          .VITE_ENV_DEFAULT_MARKET_CONFIGS,
       }
     : (window as any).COREUM?.env;
 
 export const BASE_API_URL = env.VITE_ENV_BASE_API;
 export const WS_URL = env.VITE_ENV_WS;
+export const DEFAULT_MARKET_CONFIGS = env.VITE_ENV_DEFAULT_MARKET_CONFIGS;
